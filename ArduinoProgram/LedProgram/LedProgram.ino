b0a77e52b22e164131746227347ac1aec20b9331
caiomarqs <caiomarques1412@gmail.com>
2020-03-17 09:13:46 -0300

    Add send data to Serial Port && Fix methods

---
+++ ArduinoProgram/LedProgram/LedProgram.ino
@@ -0,0 +1,27 @@
+const int pwmPin = 9;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pwmPin, OUTPUT);
+  analogWrite(pwmPin, 0);
+}
+String inString = "";
+void loop(){
+  while(Serial.available() > 0){     
+    int inChar = Serial.read();
+ 
+    if(isDigit(inChar)) {
+      inString += (char)inChar;
+    }
+    if (inChar == '\n') {
+      Serial.print("Value:");
+      Serial.print(inString.toInt());
+      Serial.print(" | String: ");
+      Serial.println(inString);
+      analogWrite(pwmPin, inString.toInt());
+      inString = "";
+    }
+  }
+  delay(50);
+}
--- test/index.js
+++ test/index.js
@@ -1,15 +1,30 @@
 'use strict'
 const SerialArduinoApp = require('../lib/serial-arduino-app.js')
+const EventEmitter = require('events')
 
-const config = { 
-    log: false, 
-    port: 'COM3', 
-    baudRate: 9600 
+// Config object 
+const config = {
+    log: false,
+    port: 'COM3',
+    baudRate: 9600,
+    serialLog: true
 }
 
 const serial = new SerialArduinoApp(config)
-serial.emmiter()
 
+serial.reciveDataToSerial() //Start recive data from serialport
 serial.on('serial-data', (serialdata) => {
     console.log(serialdata)
 })
+
+
+serial.sendDataToSerial() //Start send data to serialport
+
+const emitter = new EventEmitter() //Use to simulate the event
+emitter.on('event', () => {
+    setInterval(() => {
+        serial.emit('send-data', '100') //Emit the data to Serial Port
+    }, 50)
+})
+
+emitter.emit('event') // Force the event
